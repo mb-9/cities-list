709031bda3ad270d784aba453f69f3222787c6a7
mb-9 <kvapka.kvapla@gmail.com>
2024-02-12 11:13:42 +0100

    fixed checking return values

--- app/Models/ScraperHelper.php
+++ app/Models/ScraperHelper.php
@@ -8,12 +8,21 @@ class ScraperHelper
 {
     /**
      * @param string $url
-     * @return HtmlDocument
+     * @return HtmlDocument|null
      */
-    public static function getPageContent(string $url) : HtmlDocument {
+    public static function getPageContent(string $url) : ?HtmlDocument {
 
         $content = file_get_contents($url);
+
+        if ($content === false) {
+            return null;
+        }
+
         $content = iconv('windows-1250', 'utf-8', $content);
+
+        if($content === false){
+            return null;
+        }
 
         return new HtmlDocument($content);
     }
