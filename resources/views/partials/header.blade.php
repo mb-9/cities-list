0460d7e526f014a2b3b8479babcce4785f1b664d
mb-9 <kvapka.kvapla@gmail.com>
2024-02-13 11:10:24 +0100

    header in place

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,22 +3,23 @@
 
 <header>
     <div id="top-container" class="container">
+
         <div class="row flex-nowrap justify-content-between align-items-center">
             <div class="col-4 pt-1">
-                <img src="/assets/logo-cleaning-gardening.png">
-                <a class="text-muted" href="#">Logo will go here </a>
+
+                <a class="text-muted" href="#"><img src="/assets/images/logo-cleaning-gardening.png"> </a>
             </div>
 
             <div class="col-6 pt-1">
                 <div class="row">
 
-                    <div class="col-5 d-flex justify-content-end ">
-                        <a class="blog-header-logo text-dark" href="#">Kontakty a telefónne čísla</a>
+                    <div class="col-5 d-flex justify-content-end d-flex align-items-center">
+                        <a class="blog-header-logo a-contacts" href="#">Kontakty a čísla na oddelenia</a>
                     </div>
-                    <div class="col-2  d-flex justify-content-end ">
-                        <a class="blog-header-logo text-dark" href="#">SK</a>
+                    <div class="col-1  d-flex justify-content-end  d-flex align-items-center ">
+                        <a class="blog-header-logo language-selection" href="#">SK</a>
                     </div>
-                    <div class="col-3 d-flex justify-content-end align-items-center">
+                    <div class="col-4 d-flex justify-content-end align-items-center">
                         <form id="search-form" method="get" action="https://www.sukl.sk/hlavna-stranka/slovenska-verzia/databazy-a-servis/vyhladavanie-liekov-zdravotnickych-pomocok-a-zmien-v-liekovej-databaze/vyhladavanie-na-strankach?page_id=241" class="form-search">
                             <input type="text" name="term" class="fulltext-search__input" id="search-text" autocomplete="off" placeholder="Hľadať..." value="">
                             <input type="hidden" id="search_url" value="https://www.sukl.sk/hlavna-stranka/vysledky-vyhladavania-quicksearch?page_id=4576">
@@ -26,15 +27,34 @@
                         </form>
                     </div>
                     <div class="col-2">
-                        <button class="btn-info">Prihlásiť sa</button>
+                        <button class="btn btn-green btn-success" id="btn-login">Prihlásenie</button>
                     </div>
                 </div>
             </div>
 
 
-
-
         </div>
+
+
+            <ul class="menu">
+                <li style="">
+                    <a href="">O nás</a>
+
+                </li>
+                <li style="">
+                    <a href="">Zoznam miest</a>
+
+                </li>
+                <li style="">
+                    <a href="">Inšpekcia </a>
+
+                </li>
+                <li style="">
+                    <a href="">Kontakt </a>
+
+                </li>
+            </ul>
+
 
     </div>
 </header>
