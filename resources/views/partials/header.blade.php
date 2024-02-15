65f04a4a1d26166b25f14da49a2989acab5cf458
mb-9 <kvapka.kvapla@gmail.com>
2024-02-15 15:05:09 +0100

    fixed header on small screens

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,35 +2,35 @@
 
 
 <header>
+
+
     <div id="top-container" class="container">
 
-        <div class="row flex-nowrap justify-content-between align-items-center">
-            <div class="col-4 pt-1">
+        <div class="row g-2 align-items-center">
+            <div class="col-xl-5 col-lg-5 col-md col-sm pt-1">
 
                 <a class="text-muted" href="#"><img src="/assets/images/logo-cleaning-gardening.png"> </a>
             </div>
 
-            <div class="col-6 pt-1">
-                <div class="row">
-
-                    <div class="col-5 d-flex justify-content-end d-flex align-items-center">
-                        <a class="blog-header-logo a-contacts" href="#">Kontakty a čísla na oddelenia</a>
-                    </div>
-                    <div class="col-1  d-flex justify-content-end  d-flex align-items-center ">
-                        <a class="blog-header-logo language-selection" href="#">SK</a>
-                    </div>
-                    <div class="col-4 d-flex justify-content-end align-items-center">
-                        <form id="search-form" method="get" action="https://www.sukl.sk/hlavna-stranka/slovenska-verzia/databazy-a-servis/vyhladavanie-liekov-zdravotnickych-pomocok-a-zmien-v-liekovej-databaze/vyhladavanie-na-strankach?page_id=241" class="form-search">
-                            <input type="text" name="term" class="fulltext-search__input" id="search-text" autocomplete="off" placeholder="Hľadať..." value="">
-                            <input type="hidden" id="search_url" value="https://www.sukl.sk/hlavna-stranka/vysledky-vyhladavania-quicksearch?page_id=4576">
-
-                        </form>
-                    </div>
-                    <div class="col-2">
-                        <button class="btn btn-green btn-success" id="btn-login">Prihlásenie</button>
-                    </div>
-                </div>
+            <div class="col-xl-2 col-lg-2 col-md col-sm d-flex align-items-center">
+                <a class="blog-header-logo a-contacts" href="#">Kontakty a čísla na oddelenia</a>
             </div>
+            <div class="col-md  col-sm col-1 d-flex  align-items-center ">
+                <a class="blog-header-logo language-selection" href="#">SK</a>
+            </div>
+
+            <div class="col-xl-2 col-lg-2 col-md col-sm d-flex align-items-center">
+                <form id="search-form" method="get" action="https://www.sukl.sk/hlavna-stranka/slovenska-verzia/databazy-a-servis/vyhladavanie-liekov-zdravotnickych-pomocok-a-zmien-v-liekovej-databaze/vyhladavanie-na-strankach?page_id=241" class="form-search">
+                    <input type="text" name="term" class="fulltext-search__input" id="search-text" autocomplete="off" placeholder="Hľadať..." value="">
+                    <input type="hidden" id="search_url" value="https://www.sukl.sk/hlavna-stranka/vysledky-vyhladavania-quicksearch?page_id=4576">
+
+                </form>
+            </div>
+
+            <div class="col-xl-2 col-lg-2 col-md col-sm ">
+                <button class="btn btn-green btn-success" id="btn-login">Prihlásenie</button>
+            </div>
+
 
 
         </div>
